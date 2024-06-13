4f0c739e1e4a29110a8fa8a2412a96a5eec7f598
PhamManhCuong <cuongpham.251203@gmail.com>
2024-06-13 07:51:35 +0700

    feat: search-user

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { ENV } from "@/constant/environments";
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/authReducer";
 import popupReducer from "./reducers/popupReducer";
+import userReducer from "./reducers/userReducer";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     popup: popupReducer,
+    user: userReducer,
   },
   devTools: ENV === "development",
 });
